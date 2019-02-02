12e8c9d7dc8c5ef987377a04143bcb716ae1a562
Randy Reyes <rqreyes@gmail.com>
2019-02-01 18:31:20 -0800

    redesign initial commit

--- 404.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$pageTitle = 'Randy Q Reyes | 404 Error';
-	$robots = "<meta name='robots' content='noindex, nofollow'>";
-	$h1 = 'Sorry';
-	$subtitle = "The page you're trying to visit doesn't exist. Please check the URL or try using the navigation."; 
-	include 'includes/header.php';
-	include 'includes/body-footer.php';
-?>
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$pageTitle = 'Randy Q Reyes | Front-end Web Warrior';
-	$h1 = "Hi, I'm Randy Reyes";
-	$subtitle = 'and I love to develop websites'; 
-	include 'includes/header.php';
-	include 'includes/body-footer.php';
-?>
