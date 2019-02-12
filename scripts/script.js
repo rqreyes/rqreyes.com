d1b84563e79eed595cda0d713cd85dd5492e566e
Randy Reyes <rqreyes@gmail.com>
2019-02-11 19:08:25 -0800

    add project background transitions

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
+    var bg1 = "#fdb812",
+        bg2 = "#005ba4",
+        bg3 = "#0093c5",
+        bg4 = "#00261c";
 
+        $(window).on("scroll touchmove", function () {
+        var scrollBottom = $(window).scrollTop() + $(window).height();
+
+        if (scrollBottom >= $("#bg1").offset().top) {
+            $('body').css('background', bg1);
+        };
+        if (scrollBottom > $("#bg2").offset().top) {
+            $('body').css('background', bg2)
+        };
+        if (scrollBottom > $("#bg3").offset().top) {
+            $('body').css('background', bg3)
+        };
+        if (scrollBottom > $("#bg4").offset().top) {
+            $('body').css('background', bg4)
+        };
+    });
 });
